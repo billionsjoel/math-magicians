986d807768373b5b41856331460f02c31fb87b22
Billions Joel <joelatugonza@gmail.com>
2022-02-07 17:20:40 +0300

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<div className="grid-container calc-container">
-				<div class="grid-item item-1 answer-field">0</div>
-				<div class="grid-item">AC</div>
-				<div class="grid-item">&#177;</div>
-				<div class="grid-item">%</div>
-				<div class="grid-item item-operations">&divide;</div>
-				<div class="grid-item">7</div>
-				<div class="grid-item">8</div>
-				<div class="grid-item">9</div>
-				<div class="grid-item item-operations">&times;</div>
-				<div class="grid-item">4</div>
-				<div class="grid-item">5</div>
-				<div class="grid-item">6</div>
-				<div class="grid-item item-operations">&minus;</div>
-				<div class="grid-item">1</div>
-				<div class="grid-item">2</div>
-				<div class="grid-item">3</div>
-				<div class="grid-item item-operations">&#43;</div>
-				<div class="grid-item item-zero">0</div>
-				<div class="grid-item">&#8729;</div>
-				<div class="grid-item item-operations">&#61;</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="grid-container calc-container">
+        <div className="grid-item item-1 answer-field">0</div>
+        <div className="grid-item">AC</div>
+        <div className="grid-item">&#177;</div>
+        <div className="grid-item">%</div>
+        <div className="grid-item item-operations">&divide;</div>
+        <div className="grid-item">7</div>
+        <div className="grid-item">8</div>
+        <div className="grid-item">9</div>
+        <div className="grid-item item-operations">&times;</div>
+        <div className="grid-item">4</div>
+        <div className="grid-item">5</div>
+        <div className="grid-item">6</div>
+        <div className="grid-item item-operations">&minus;</div>
+        <div className="grid-item">1</div>
+        <div className="grid-item">2</div>
+        <div className="grid-item">3</div>
+        <div className="grid-item item-operations">&#43;</div>
+        <div className="grid-item item-zero">0</div>
+        <div className="grid-item">&#8729;</div>
+        <div className="grid-item item-operations">&#61;</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
