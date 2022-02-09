fab6ac3080cdfbe0404d6f42b81c0825eeae84fe
Billions Joel <joelatugonza@gmail.com>
2022-02-09 11:54:18 +0300

    Map each button with class and operation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import { useState } from 'react';
 import Calculator from './components/Calculator';
 
 function App() {
-  const [answer, setAnswer] = useState(32);
   return (
     <>
       <div className="App">
-        <Calculator answer={answer} handleCalc={setAnswer} />
+        <Calculator />
       </div>
     </>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
 import './Calculator.css';
 import React from 'react';
 import calculate from '../logic/calculate';
@@ -13,45 +15,60 @@ class Calculator extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = (e) => {
-    if (!e.target.name) return;
-    const { next, total, operation } = calculate(this.state, e.target.name);
+ handleClick = (e) => {
+   if (!e.target.name) return;
+   const { next, total, operation } = calculate(this.state, e.target.name);
 
-    if (next === null && total === null) {
-      this.setState({ next, total: '0', operation });
-    } else {
-      this.setState({ next, total, operation });
-    }
-  }
+   if (next === null && total === null) {
+     this.setState({ next, total: '0', operation });
+   } else {
+     this.setState({ next, total, operation });
+   }
+ };
 
-  render() {
-    return (
-      <div className="App">
-        <div className="grid-container calc-container">
-          <div className="grid-item item-1 answer-field">{this.state}</div>
-          <div className="grid-item">AC</div>
-          <div className="grid-item">&#177;</div>
-          <div className="grid-item">%</div>
-          <div className="grid-item item-operations">&divide;</div>
-          <div className="grid-item">7</div>
-          <div className="grid-item">8</div>
-          <div className="grid-item">9</div>
-          <div className="grid-item item-operations">&times;</div>
-          <div className="grid-item">4</div>
-          <div className="grid-item">5</div>
-          <div className="grid-item">6</div>
-          <div className="grid-item item-operations">&minus;</div>
-          <div className="grid-item">1</div>
-          <div className="grid-item">2</div>
-          <div className="grid-item">3</div>
-          <div className="grid-item item-operations">&#43;</div>
-          <div className="grid-item item-zero">0</div>
-          <div className="grid-item">&#8729;</div>
-          <div className="grid-item item-operations">&#61;</div>
-        </div>
-      </div>
-    );
-  }
+ render() {
+   const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+   const operators = '÷x-+=';
+   const { total, next } = this.state;
+
+   return (
+     <div className="App">
+       <div className="grid-container">
+         {total ? (
+           <div className="input input grid-item item-1 answer-field">
+             {total}
+           </div>
+         ) : (
+           <div className="input input grid-item item-1 answer-field">
+             {next}
+           </div>
+				 )}
+         {buttons.map((btn) => (
+           <button
+             onClick={this.handleClick}
+             type="button"
+             key={btn}
+             id={btn}
+             name={btn}
+             className={`grid-item ${
+							  operators.includes(btn) ? 'operator' : ''
+             } ${btn === '0' ? 'item-zero' : ''} ${
+							  btn === '÷'
+								|| btn === 'x'
+								|| btn === '-'
+								|| btn === '+'
+								|| btn === '='
+							    ? 'item-operations'
+							    : ''
+             }`}
+           >
+             {btn}
+           </button>
+         ))}
+       </div>
+     </div>
+   );
+ }
 }
 
 export default Calculator;
