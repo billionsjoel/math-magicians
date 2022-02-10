52b713053e954ba8a0c19e24a33902d3a86447ab
Billions Joel <joelatugonza@gmail.com>
2022-02-10 12:50:59 +0300

    Use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,74 +1,66 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-tabs */
 import './Calculator.css';
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
 
- handleClick = (e) => {
-   if (!e.target.name) return;
-   const { next, total, operation } = calculate(this.state, e.target.name);
+  const handleClick = (e) => {
+    if (!e.target.name) return;
+    const { next, total, operation } = calculate(state, e.target.name);
 
-   if (next === null && total === null) {
-     this.setState({ next, total: '0', operation });
-   } else {
-     this.setState({ next, total, operation });
-   }
- };
+    if (next === null && total === null) {
+      setState({ next, total: '0', operation });
+    } else {
+      setState({ next, total, operation });
+    }
+  };
 
- render() {
-   const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-   const operators = '÷x-+=';
-   const { total, next } = this.state;
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const operators = '÷x-+=';
+  const { total, next } = state;
 
-   return (
-     <div className="App">
-       <div className="grid-container">
-         {total ? (
-           <div className="input input grid-item item-1 answer-field">
-             {total}
-           </div>
-         ) : (
-           <div className="input input grid-item item-1 answer-field">
-             {next}
-           </div>
-				 )}
-         {buttons.map((btn) => (
-           <button
-             onClick={this.handleClick}
-             type="button"
-             key={btn}
-             id={btn}
-             name={btn}
-             className={`grid-item ${
-							  operators.includes(btn) ? 'operator' : ''
-             } ${btn === '0' ? 'item-zero' : ''} ${
-							  btn === '÷'
+  return (
+    <div className="App">
+      <div className="grid-container">
+        {total ? (
+          <div className="input input grid-item item-1 answer-field">
+            {total}
+          </div>
+        ) : (
+          <div className="input input grid-item item-1 answer-field">
+            {next}
+          </div>
+        )}
+        {buttons.map((btn) => (
+          <button
+            onClick={handleClick}
+            type="button"
+            key={btn}
+            id={btn}
+            name={btn}
+            className={`grid-item ${operators.includes(btn) ? 'operator' : ''
+            } ${btn === '0' ? 'item-zero' : ''} ${btn === '÷'
 								|| btn === 'x'
 								|| btn === '-'
 								|| btn === '+'
 								|| btn === '='
-							    ? 'item-operations'
-							    : ''
-             }`}
-           >
-             {btn}
-           </button>
-         ))}
-       </div>
-     </div>
-   );
- }
-}
+						  ? 'item-operations'
+						  : ''
+            }`}
+          >
+            {btn}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
