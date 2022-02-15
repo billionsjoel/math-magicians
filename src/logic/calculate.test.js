f7ee5b1b7e68b535394088dfbcce0f71a7c15b9c
Billions Joel <joelatugonza@gmail.com>
2022-02-15 22:05:25 +0300

    Expectation

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,5 +1,14 @@
 import calculate from './calculate';
 
 test('should first', () => {
-
+	expect(
+		calculate(
+			{
+				total: null,
+				next: null,
+				operation: null,
+			},
+			'AC'
+		)
+	)
 });
