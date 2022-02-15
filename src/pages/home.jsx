f809b8ce16800473fb8c62f100df5112b0566cef
Billions Joel <joelatugonza@gmail.com>
2022-02-15 16:32:06 +0300

    Add paragraph on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,12 @@ function Home() {
 	return (
   <div>
     <h1>Welcome to our page!</h1>
-
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+      cupiditate quisquam aspernatur facere, ipsum autem possimus ad cum
+      voluptatum et iusto ullam iure maiores quas numquam non unde temporibus
+      dolore?
+    </p>
   </div>
 	);
 }
