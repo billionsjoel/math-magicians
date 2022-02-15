1976b481df21fc3d4aa044fb8363e9afd8628a24
Billions Joel <joelatugonza@gmail.com>
2022-02-15 22:20:18 +0300

    Create tree

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -2,4 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from './Calculator';
 
+it('renders buttons', () => {
+	const tree = renderer.create(<Calculator />).toJSON();
 
+});
