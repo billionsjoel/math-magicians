20ff3f11a59849ba4efa6f44a0f2500aeaaf3c87
Billions Joel <joelatugonza@gmail.com>
2022-02-15 22:06:08 +0300

    Fix indention

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,18 +1,18 @@
 import calculate from './calculate';
 
 test('should first', () => {
-	expect(
-		calculate(
-			{
-				total: null,
-				next: null,
-				operation: null,
-			},
-			'AC'
-		)
-	).toMatchObject({
-		total: null,
-		next: null,
-		operation: null,
-	});
+  expect(
+    calculate(
+      {
+        total: null,
+        next: null,
+        operation: null,
+      },
+      'AC',
+    ),
+  ).toMatchObject({
+    total: null,
+    next: null,
+    operation: null,
+  });
 });
