f395a37f936a81528032d062db1473e5a02b81c6
Billions Joel <joelatugonza@gmail.com>
2022-02-15 23:47:33 +0300

    Import jest dom

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { render, fireEvent, screen } from '@testing-library/react';
-
+import '@testing-library/jest-dom';
 
 
 
