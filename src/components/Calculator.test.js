1b81df546ee772073e4f59d85adf42c1c1cf097c
Billions Joel <joelatugonza@gmail.com>
2022-02-15 22:21:12 +0300

    Fix indention

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import Calculator from './Calculator';
 
 it('renders buttons', () => {
-	const tree = renderer.create(<Calculator />).toJSON();
+  const tree = renderer.create(<Calculator />).toJSON();
 
-	expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
