7bdb8245f5c8d5d78745ec6793b74ff286c7cf8c
Billions Joel <joelatugonza@gmail.com>
2022-02-15 15:24:56 +0300

    Install react router

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+import { Link } from 'react-router-dom';
+
+function home() {
+	return <div>Home Page</div>;
+}
+
+export default home;
---
+++ src/pages/quote.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+import { Link } from 'react-router-dom';
+
+function quote() {
+	return <div>Quote page</div>;
+}
+
+export default quote;
