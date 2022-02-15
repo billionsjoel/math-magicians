e0fe2ca98c9d7ccb9a0382d67bf1f0b4f2ecf03b
Billions Joel <joelatugonza@gmail.com>
2022-02-15 23:46:59 +0300

    Import react

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,16 +1,10 @@
 import * as React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 
-import Navbar from './Navbar';
 
-test('render nav element', () => {
-	const { container, getByText } = render(
-		<MemoryRouter>
-			<Navbar />
-		</MemoryRouter>
-	);
-	expect(getByText('Math Magicians')).toBeInTheDocument();
-});
+
+
+
+
+
+
