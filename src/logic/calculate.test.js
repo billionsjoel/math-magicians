1500c0eefede7e001caeea4d00dd54c3d93f94b6
Billions Joel <joelatugonza@gmail.com>
2022-02-16 11:26:27 +0300

    Add describe method on calculate

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,18 +1,22 @@
 import calculate from './calculate';
 
-test('should first', () => {
-  expect(
-    calculate(
-      {
-        total: null,
-        next: null,
-        operation: null,
-      },
-      'AC',
-    ),
-  ).toMatchObject({
-    total: null,
-    next: null,
-    operation: null,
+describe('testing calculate.js', () =>
+{
+  test('should first', () =>
+  {
+    expect(
+      calculate(
+        {
+          total: null,
+          next: null,
+          operation: null,
+        },
+        'AC',
+      ),
+    ).toMatchObject({
+      total: null,
+      next: null,
+      operation: null,
+    });
   });
-});
+}
