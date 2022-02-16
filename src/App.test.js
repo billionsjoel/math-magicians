ca382bc70b7dad34c1134161760e42634ddbe5cf
Billions Joel <joelatugonza@gmail.com>
2022-02-16 10:35:50 +0300

    Fix tabs

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,9 @@ import '@testing-library/jest-dom';
 
 import Home from './pages/Home';
 
-test('render homepage', () =>
-{
-	render(<Home />);
-	expect(screen.getByText('Welcome to our page!')).toMatchInlineSnapshot(`
+test('render homepage', () => {
+  render(<Home />);
+  expect(screen.getByText('Welcome to our page!')).toMatchInlineSnapshot(`
 <h1>
   Welcome to our page!
 </h1>
