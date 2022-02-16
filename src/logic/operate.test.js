242790a4b073623865607bda77d935f22790a459
Billions Joel <joelatugonza@gmail.com>
2022-02-16 11:47:51 +0300

    Add subtraction test

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,5 +1,11 @@
 import operate from './operate';
 
-test('Summation of 2 numbers', () => {
-  expect(operate(2, 3, '+')).toBe('5');
+describe('testing operate.js', () => {
+  test('checks summation of 2 numbers', () => {
+    expect(operate(2, 3, '+')).toBe('5');
+  });
+
+  test('checks subtraction of 2 numbers', () => {
+		expect(operate(10, 5, '-')).toBe('5');
+	});
 });
