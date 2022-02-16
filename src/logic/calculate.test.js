ab92518a4283979de155f25329033ff4d30f1b65
Billions Joel <joelatugonza@gmail.com>
2022-02-16 11:37:21 +0300

    Write test for second number

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -37,4 +37,16 @@ describe('testing calculate.js', () => {
     const result = calculate(obj, '+');
     expect(result.operation).toEqual('+');
   });
+
+  test('checks if clicked on second number after operation', () => {
+		const obj = {
+			total: '5',
+			next: null,
+			operation: '-',
+		};
+		const result = calculate(obj, '2');
+		expect(result.total).toEqual('5');
+		expect(result.next).toEqual('2');
+		expect(result.operation).toEqual('-');
+	});
 });
