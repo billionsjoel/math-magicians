275f8720e25c3b0ee8b3bb2b6e78c1372069bd1f
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-16 00:01:08 -0800

    Add test for homepage

--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,11 @@ import '@testing-library/jest-dom';
 
 import Home from './pages/Home';
 
-
+test('render homepage', () => {
+    render( < Home / > );
+    expect(screen.getByText('Welcome to our page!')).toMatchInlineSnapshot(`
+<h1>
+  Welcome to our page!
+</h1>
+`);
+});
