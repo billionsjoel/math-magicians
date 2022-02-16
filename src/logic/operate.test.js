93593fe35254db64a8f24833d109a708949bcb09
Billions Joel <joelatugonza@gmail.com>
2022-02-16 11:50:26 +0300

    Add division test

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,16 +1,19 @@
 import operate from './operate';
 
 describe('testing operate.js', () => {
-  test('checks summation of 2 numbers', () => {
-    expect(operate(2, 3, '+')).toBe('5');
-  });
+	test('checks summation of 2 numbers', () => {
+		expect(operate(2, 3, '+')).toBe('5');
+	});
 
-  test('checks subtraction of 2 numbers', () => {
+	test('checks subtraction of 2 numbers', () => {
 		expect(operate(10, 5, '-')).toBe('5');
-  });
+	});
 
-  test('checks multiplication of 2 numbers', () => {
+	test('checks multiplication of 2 numbers', () => {
 		expect(operate(10, 2, 'x')).toBe('20');
-  });
+	});
 
+	test('checks division of 2 numbers', () => {
+		expect(operate(100, 5, '÷')).toBe('20');
+	});
 });
