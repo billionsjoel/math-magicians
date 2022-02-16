f7e4bb7d10594a96c6ba0889ed87658294b7bd14
Billions Joel <joelatugonza@gmail.com>
2022-02-16 10:48:00 +0300

    Remove test

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,4 @@ import '@testing-library/jest-dom';
 
 import Home from './pages/Home';
 
-test('render homepage', () => {
-  render(<Home />);
-  expect(screen.getByText('Welcome to our page!')).toMatchInlineSnapshot(`
-<h1>
-  Welcome to our page!
-</h1>
-`);
-});
+
